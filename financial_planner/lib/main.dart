b9ef465addcbfe5d63167668ec143d84714b7d3c
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-04-26 11:22:44 -0400

    set up firebase database connection

--- financial_planner/lib/main.dart
+++ financial_planner/lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:financial_planner/homeScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +19,101 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final Stream<QuerySnapshot> _userStream =
+  FirebaseFirestore.instance.collection('Users').snapshots();
+  CollectionReference users = FirebaseFirestore.instance.collection('Users');
+  String name = '';
+
+  Future<void> addUser() {
+    return users
+        .add({
+      'name': name,
+    })
+        .then((value) => print("user added"))
+        .catchError((error) => print("failed to add the user "));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Firestore Demo'),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.zero,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+
+              children: [
+                Text(
+                  'Enter user name',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                TextField(
+                  onChanged: (value) {
+                    name = value;
+                  },
+                  decoration: InputDecoration(
+                      hintText: 'This could be your password',
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                      )),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      addUser();
+                    },
+                    child: Text('Adding user to the cloud')),
+                SizedBox(
+                  height: 10.0,
+                ),
+                StreamBuilder<QuerySnapshot>(
+                    stream: _userStream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return Text('something went wrong');
+                      }
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Text('Loading');
+                      }
+                      return ListView(
+                        shrinkWrap: true,
+                        children: snapshot.data!.docs
+                            .map((DocumentSnapshot document) {
+                          Map<String, dynamic> data =
+                          document.data()! as Map<String, dynamic>;
+                          return ListTile(
+                            title: Text(data['name']),
+                          );
+                        }).toList(),
+                      );
+                    }),
+              ],
+            ),
+          ),
+        ));
   }
 }
