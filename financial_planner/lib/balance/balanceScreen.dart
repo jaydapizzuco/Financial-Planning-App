ae43aca6fe08ae4e3f05423c2187122631f35000
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-04-27 15:28:44 -0400

    displaying balance on balanceScreen page

--- financial_planner/lib/balance/balanceScreen.dart
+++ financial_planner/lib/balance/balanceScreen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
+import '../models/Balance.dart';
 
 
 class BalanceScreen extends StatefulWidget {
@@ -13,10 +16,71 @@ class BalanceScreen extends StatefulWidget {
 }
 
 class _BalanceScreenState extends State<BalanceScreen> {
+  num? balanceAmount;
+
+  @override
+  void initState()  {
+    super.initState();
+    setBalance();
+  }
+
+  void setBalance() async {
+    num? amount = await getBalanceById(widget.userId);
+    setState(() {
+      balanceAmount = amount;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child:  Text(" Balance Screen ${widget.userId}", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Account Balance"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 200,
+                width: 350,
+                decoration: BoxDecoration(
+                  color: Colors.blue[100],
+                    borderRadius: BorderRadius.all(Radius.circular(20))
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text("Account Balance", style: TextStyle(fontSize: 30, color: Colors.blueGrey, fontWeight: FontWeight.bold),),
+                    Text("\$\ ${balanceAmount}", style: TextStyle(fontSize: 50, color: Colors.blueGrey, fontWeight: FontWeight.bold),)
+                  ],
+                )
+                )
+          ],
+        ),
+      )
     );
+  }
+
+
+  Future<num?> getBalanceById(String? id) async {
+    num? balanceAmount;
+
+    try {
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('Balances')
+          .where('userId', isEqualTo: widget.userId)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        Balance balance = Balance.fromSnapshot(querySnapshot.docs.first);
+        balanceAmount = balance.amount;
+      }
+    } catch (error) {
+      print('Error getting ID: $error');
+    }
+
+    return balanceAmount;
   }
 }
