ab1da8db3b819eb681921b8084759f0a199d8327
yumvinegar <horse.shoe@outlook.com>
2024-04-26 23:29:45 -0400

    did a basic placement of elements on the balanceScreen

--- financial_planner/lib/balance/balanceScreen.dart
+++ financial_planner/lib/balance/balanceScreen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BalanceScreen extends StatefulWidget {
   const BalanceScreen({super.key});
@@ -10,6 +12,46 @@ class BalanceScreen extends StatefulWidget {
 class _BalanceScreenState extends State<BalanceScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Finances'),
+      ),
+
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+
+              //ListView for notifications
+              ListView(),
+
+              SizedBox(height: 20,),
+
+              //Container for the tree
+              Container(
+                height: 400,
+                width: 300,
+                color: Colors.grey,
+              ),
+
+              SizedBox(height: 20,),
+
+              //Container for the account balance
+              Container(
+                width: 300,
+                height: 50,
+                child: Text('Account balance: '),
+              )
+
+            ],
+          ),
+
+          //need to add a menu as well 
+        ),
+      ),
+    );
   }
 }
