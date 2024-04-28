3b76e8997dff33dd861eeae71e1181c96b8e6aa8
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-04-28 17:31:26 -0400

    displaying budgets

--- financial_planner/lib/budget/budgetScreen.dart
+++ financial_planner/lib/budget/budgetScreen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'addBudget.dart';
@@ -13,6 +14,22 @@ class BudgetScreen extends StatefulWidget {
 }
 
 class _BudgetScreenState extends State<BudgetScreen> {
+  Stream<QuerySnapshot>? _budgetStream;
+
+  @override
+  void initState() {
+    super.initState();
+    getBudgets();
+  }
+
+  void getBudgets() async {
+    Stream<QuerySnapshot>? _budgets = await _getBudgets(widget.userId);
+
+    setState(() {
+      _budgetStream = _budgets;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +42,63 @@ class _BudgetScreenState extends State<BudgetScreen> {
     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
     crossAxisAlignment: CrossAxisAlignment.center,
     children: [
-      SizedBox(height: 10,),
+      StreamBuilder<QuerySnapshot>(
+          stream: _budgetStream,
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('something went wrong');
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text('Loading');
+            }
+            return Expanded(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: snapshot.data!.docs
+                      .map((DocumentSnapshot document) {
+                    Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                    return
+                      Container(
+                          width: 200,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                             // GestureDetector(
+                             //    child:
+                            Container(height: 100,
+                                  width: 350,
+                                  decoration: BoxDecoration(
+                                      color: Colors.yellowAccent[100],
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(20))
+                                  ),
+                                  child: ListTile(
+                                      title: Text(data['name'] + "\n\$\ " +
+                                          (data['amount'] - data['amountUsed']).toString() +" remaining",
+                                        style: TextStyle(fontSize: 28, ),),
+                                      subtitle: Text(data['description'])
+                                  ),
+                                ),
+
+                              //   onTap: (){
+                              //     Navigator.push(context, MaterialPageRoute(builder: (context) => IncomeInfo(id: data['id'])));
+                              //     ScaffoldMessenger.of(context).showSnackBar(
+                              //       const SnackBar(
+                              //         content: Text('A SnackBar has been shown.'),
+                              //       ),
+                              //     );
+                              //   },
+                              // ),
+                            ],
+                          )
+                      );
+                  }).toList(),
+                )
+            );
+          }),
       SizedBox(
           height: 50,
           width: 300,
@@ -42,4 +115,12 @@ class _BudgetScreenState extends State<BudgetScreen> {
     );
   }
 
+  Future <Stream<QuerySnapshot>> _getBudgets(String? id) async {
+    Stream<QuerySnapshot> budgets = await FirebaseFirestore.instance
+        .collection('Budgets')
+        .where('userId', isEqualTo: id)
+        .snapshots();
+
+    return budgets;
+  }
 }
