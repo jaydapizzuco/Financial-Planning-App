656f3178377e2dcfc874813a08551bf68fbfd13a
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-05-17 21:38:06 -0400

    calculating the time left before budget resets

--- financial_planner/lib/budget/budgetInfo.dart
+++ financial_planner/lib/budget/budgetInfo.dart
@@ -17,6 +17,15 @@ class BudgetInfo extends StatefulWidget {
 class _BudgetInfoState extends State<BudgetInfo> {
   Stream<QuerySnapshot>? _budget;
 
+  DateTime today = DateTime.now();
+  int? remainingTime;
+  String? timeLeft = "";
+  String? timeUnit;
+  int? yearsRemaining = 0;
+  int? monthsRemaining = 0;
+  int? weeksRemaining = 0;
+  int? daysRemaining = 0;
+
   @override
   void initState() {
     super.initState();
@@ -51,6 +60,107 @@ class _BudgetInfoState extends State<BudgetInfo> {
           }
           DocumentSnapshot document = snapshot.data!.docs.first;
           Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+
+          if(data['endDate'] != null) {
+
+            int years = data['endDate'].toDate().year - today.year;
+            int months = data['endDate'].toDate().month - today.month;
+            int days = data['endDate'].toDate().day - today.day;
+
+            if (days < 0) {
+              final previousMonthDate = DateTime(data['endDate'].toDate().year, data['endDate'].toDate().month, 0);
+              days += previousMonthDate.day;
+              months -= 1;
+            }
+
+            if (months < 0) {
+              months += 12;
+              years -= 1;
+            }
+
+            if (years < 0) {
+              months = 0;
+              days = data['endDate'].toDate().difference(today).inDays;
+              years = 0;
+            }
+
+            int totalDaysDifference = data['endDate'].toDate().difference(today).inDays;
+            int totalWeeks = totalDaysDifference ~/ 7;
+            int weeks = totalWeeks % 4;
+            days = totalDaysDifference % 7;
+
+            if(years > 0){
+              if(years == 1){
+                timeLeft = "$years Year";
+              }
+              else {
+                timeLeft = "$years Years";
+              }
+            }
+            if(months > 0){
+              if(months == 1){
+                timeLeft = "$timeLeft $months Month";
+              }
+              else {
+                timeLeft = "$timeLeft $months Months";
+              }
+            }
+            if(weeks > 0) {
+              if(weeks == 1){
+                timeLeft = "$timeLeft $weeks Week";
+              }
+              else {
+                timeLeft = "$timeLeft $weeks Weeks";
+              }
+            }
+            if(days > 0) {
+              if (days == 1) {
+                timeLeft = "$timeLeft $days Day";
+              }
+              else {
+                timeLeft = "$timeLeft $days Days";
+              }
+            }
+            timeLeft = "$timeLeft Until Budget Resets";
+
+            //
+            // switch (data['unitOfTime']) {
+            //   case 'Days':
+            //     remainingTime = data['endDate']
+            //         .toDate()
+            //         .difference(today)
+            //         .inDays;
+            //   case 'Weeks':
+            //     remainingTime = data['endDate']
+            //         .toDate()
+            //         .difference(today)
+            //         .inDays ~/ 7;
+            //   case 'Months':
+            //     int yearDiff = data['endDate']
+            //         .toDate()
+            //         .year - today.year;
+            //     int monthDiff = data['endDate']
+            //         .toDate()
+            //         .month - today.month;
+            //     remainingTime = yearDiff * 12 + monthDiff;
+            //   default:
+            //     throw ArgumentError('Invalid unit of time');
+            // }
+
+            //
+            // if (remainingTime == 1) {
+            //   timeUnit =
+            //       data['unitOfTime'].toString().substring(0, data['unitOfTime']
+            //           .toString()
+            //           .length - 1);
+            // }
+            // else {
+            //   timeUnit = data['unitOfTime'];
+            // }
+            //
+            //
+            // timeLeft = "$remainingTime  $timeUnit until budget resets";
+          }
 
           Map<String,double> dataMap = {
             'Remaining' : (data['amount'] - data['amountUsed']) as double,
@@ -109,7 +219,7 @@ class _BudgetInfoState extends State<BudgetInfo> {
                         legendTextStyle: TextStyle(fontSize: 20)
                       ),
                     ),
-
+                    Text("$timeLeft", style: TextStyle(fontSize: 24),),
                     SizedBox(height: 20,),
                     SizedBox(
                         height: 50,
