c6f952e0a1f9259859437417e97f7bc27d85b1c4
Jayda Pizzuco <jaydapizzuco@gmail.com>
2024-05-22 18:47:50 -0400

    deleted unused files

--- financial_planner/lib/spendings/spendingsScreen.dart
+++ financial_planner/lib/spendings/spendingsScreen.dart
@@ -1,7 +1,6 @@
 import 'package:financial_planner/spendings/addSpending.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:financial_planner/balance/incomeInfo.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 
